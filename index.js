e220ade50597a581740d9c451b7d8830b591bd6b
Chris Thoma <chris.thoma@willowtreeapps.com>
2014-02-15 18:25:14 -0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+//statement that grabs the server file from the current directory
+Omar Mad.
+// var server = require("./server");
+// var router = require("./router");
+// var requestHandlers = require("./requestHandlers");
+
+// //this creates a blank object named handle
+// var handle = {};
+
+// handle["/"] = requestHandlers.start;
+// handle["/start"] = requestHandlers.start;
+// handle["/find"] = requestHandlers.find;
+// handle["/upload"] = requestHandlers.upload;
+// handle["/show"] = requestHandlers.show;
+
+// //pass the function route into the server
+// server.start(router.route, handle);
