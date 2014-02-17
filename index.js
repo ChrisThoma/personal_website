ff1ec4b0491f2b84017e98b884b6a55dd02d3629
Chris Thoma <chris.thoma@willowtreeapps.com>
2014-02-16 20:44:09 -0500

    nothing impossible go?

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Gotta Go Fast
-  	\n \<Nothing Impossible Go\>');
+  res.send('Gotta Go Fast\n \<Nothing Impossible Go\>');
 });
 
 var port = Number(process.env.PORT || 5000);
