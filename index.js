e0858af4392ee97f989aafdb73f900d507037863
Chris Thoma <chris.thoma@willowtreeapps.com>
2014-02-17 20:00:19 -0500

    connor image.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //route for everything else
 app.get('*', function(req, res){
-	res.send('sup');
+	res.send('<img src = "https://scontent-b-lga.xx.fbcdn.net/hphotos-ash4/319534_2085753874721_2145877264_n.jpg">');
 });
 
 //fire
